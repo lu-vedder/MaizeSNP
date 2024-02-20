7b2f339b0ee6c6da0f637b780ecf047037503106
Lucia Vedder <117737637+lu-vedder@users.noreply.github.com>
2024-02-20 11:50:53 +0100

    Update documentation/comments

--- collect_gene_ids_from_gff_v4.py
+++ collect_gene_ids_from_gff_v4.py
@@ -1,21 +1,24 @@
-#==============================================================================
-# 
-# Collecting the gene IDs of the protein coding genes of Zea mays based on v4
-# Input file: C:\Users\Lucia\Zea mays\reference\GCF_000005005.2_B73_RefGen_v4_genomic.gff
-# Output file: protein_coding_genes_v4.tsv
-# 
-# Method adapted from: collect_gene_ids_from_gtf.py
-# 
-# @author: Lucia
-# @date: 8 June 2017
-# 
-#==============================================================================
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Python3.5: collect_gene_ids_from_gff_v4.py
+
+Collecting the gene IDs of the protein-coding genes of Zea mays (reference genome v4, GFF format)
+Method adapted from: collect_gene_ids_from_gtf.py
+
+Input:  GCF_000005005.2_B73_RefGen_v4_genomic.gff (reference genome annotation, GFF format)
+Output: protein_coding_genes_v4.tsv (gene set, TSV format)
+
+@author: Lucia Vedder
+@date: 2017, June 8
+Copyright (c) Lucia Vedder 2024
+"""
 
 from optparse import OptionParser
 
 
+# Collect the IDs of the protein-coding genes and write them to output (incl. chromosome & position)
 def collect_gene_ids(infile, outfile):
-    
     with open(outfile, 'w') as out:
         out.write('#gene_id\tgene_name\tchromosome\tstart_pos\tend_pos\n')
     
@@ -49,10 +52,9 @@ def collect_gene_ids(infile, outfile):
                         out.write(chromosome), out.write('\t')
                         out.write(start_pos), out.write('\t')
                         out.write (end_pos), out.write('\n')
-                
-                
-                
 
+
+# Run script
 def main():
     
     # handle the command line arguments
@@ -62,6 +64,7 @@ def main():
     
     # start the computaions
     collect_gene_ids(args[0], args[1])
+
 
 
 if __name__ == "__main__":
